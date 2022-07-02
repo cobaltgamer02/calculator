b6dee8d82298c62d3b20791f8725a0c0fdd6b210
Goutham Sajeeth <cobaltgamer9@gmail.com>
2022-07-02 17:54:04 +0530

    second commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
 
     private var input: TextView? = null
 
+    private var lastNum : Boolean = false
+
+    private var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,10 +23,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view : View){
+        lastNum = true
+        lastDot = false
         input?.append((view as Button).text)
     }
 
     fun onClear(view : View){
         input?.text = ""
+    }
+
+    fun whetherDecimalNeeded(view: View){
+        if(lastNum && !lastDot){
+            input?.append(".")
+            lastNum = false
+            lastDot = true
+        }
     }
 }
